418d4f6a627a7e0871d6d862767bf129ba8dc1e5
Menencia <menencia@gmail.com>
2017-11-15 19:44:39 +0100

    add !ffbecron and !ffbetoplast

--- bot.js
+++ bot.js
@@ -7,9 +7,9 @@ var Redis = require('ioredis');
 var redis = new Redis(process.env.REDIS_URL);
 
 bot.on('ready', function () {
-    redis.get('top-current', function(err, current) {
-        if (!current) {
-            redis.set('top-current', JSON.stringify([]));        
+    redis.get('top-current', function(err, data) {
+        if (!data) {
+            resetTopCurrent();        
         }
     })
 });
@@ -17,26 +17,53 @@ bot.on('ready', function () {
 bot.on('message', function (message) {
 
     // detect if it's a command (not count in top)
-    if (message.content === '!ffbetop') {
-        redis.get('top-current',function(err, current) {
-            current = JSON.parse(current);
+    if (message.content === '!ffbetopcurrent') {
+        redis.get('top-current',function(err, data) {
+            data = JSON.parse(data);
             // pick 10 first
-            current = _.take(current, 10);
+            data = _.take(data, 10);
             // prettify
-            var html = ' ' + "\n" + '** TOP **' + "\n";
-            _.forEach(current, function(user, idx) {
+            var html = ' ' + "\n" + '** TOP CURRENT **' + "\n";
+            _.forEach(data, function(user, idx) {
                 html += '[' + (idx+1) + '] ' + user.name + ' (' + user.pts + 'pts)' + "\n";
             });
             message.channel.send(html);
         });
-    } else if (message.content === '!ffbeclear') {
-        redis.set('top-current', JSON.stringify([]));
-        message.channel.send('Cleared!');
+    } else if (message.content === '!ffbetoplast') {
+        redis.get('top-last',function(err, data) {
+            if (data) {
+                data = JSON.parse(data);
+                // prettify
+                var html = ' ' + "\n" + '** TOP LAST **' + "\n";
+                _.forEach(data, function(user, idx) {
+                    html += '[' + (idx+1) + '] ' + user.name + ' (' + user.pts + 'pts)' + "\n";
+                });
+            } else {
+                var html = 'No Top Last found!';
+            }
+            message.channel.send(html);
+        
+        });
+    } else if (message.content === '!ffbecron') {
+        redis.get('top-current',function(err, data) {
+            //resetTopCurrent();
+            data = JSON.parse(data);
+            // pick 10 first
+            data = _.take(data, 10);
+            buildTopLast(data, function() {
+                message.channel.send('Current->Last! (current not touched)');        
+            });
+        });
+    } else if (message.content === '!ffbecurrentclear') {
+        resetTopCurrent();
+        message.channel.send('Top Current Cleared!');
+    } else if (message.content === '!ffbelastclear') {
+        resetTopLast();
+        message.channel.send('Top Last Cleared!');
     } else if (!message.author.bot) {
-        // LOOP !!!
         // update top
-        redis.get('top-current', function(err, current) {
-            updateTopCurrent(current, message.author.username);
+        redis.get('top-current', function(err, data) {
+            updateTopCurrent(data, message.author.username);
         });
     }
 });
@@ -73,4 +100,47 @@ function updateTopCurrent(current, name) {
     }
     // save
     redis.set('top-current', JSON.stringify(current));
+}
+
+function buildTopLast(data, cb) {
+    redis.get('top-last', function(err, last) {
+        var tmp = [];
+        _.forEach(data, function(user) {
+            tmp.push({
+                name: user.name,
+                pts: user.pts,
+                pos: 'N'
+            });
+        });
+        if (last) {
+            last = JSON.parse(last);
+            addPosToLast(tmp, last);
+        }
+        redis.set('top-last', JSON.stringify(tmp));
+        cb();
+    })
+}
+
+function addPosToLast(tmp, last) {
+    _.forEach(tmp, function(user, idx){
+        var found = _.findIndex(last, {name: user.name});
+        if (found > -1) {
+            var diff = idx - found;
+            if (diff === 0) {
+                user.pos = '=';
+            } else if (diff > 0) {
+                user.pos = '+' + diff;
+            } else if (diff < 0) {
+                user.pos = '-' + diff;
+            }
+        }
+    });
+}
+
+function resetTopCurrent() {
+    redis.set('top-current', JSON.stringify([]));
+}
+
+function resetLastCurrent() {
+    redis.del('top-last');
 }
