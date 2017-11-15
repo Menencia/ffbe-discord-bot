35ab5512160370d19a3ecec0519e1bd4df386232
Menencia <menencia@gmail.com>
2017-11-15 21:47:20 +0100

    translate labels, fix cron timezone & commands

--- bot.js
+++ bot.js
@@ -15,13 +15,13 @@ new CronJob('0 0 * * *', function() {
             // pick 10 first
             data = _.take(data, 10);
             buildTopLast(data, function() {
-                bot.channels.get('380036130864758785').send('Current->Last!');   
+                bot.channels.get('380036130864758785').send('Le classement a été mis à jour !');   
             });
         });
     } catch(e) {
         console.log(e);
     }
-}, null, true, 'America/Los_Angeles');
+}, null, true, 'Europe/Paris');
 
 bot.on('ready', function () {
     redis.get('top-current', function(err, data) {
@@ -34,39 +34,39 @@ bot.on('ready', function () {
 bot.on('message', function (message) {
 
     // detect if it's a command (not count in top)
-    if (message.content === '!ffbetopcurrent') {
+    if (message.content === '!ffbe top today') {
         redis.get('top-current',function(err, data) {
             data = JSON.parse(data);
             // pick 10 first
             data = _.take(data, 10);
             // prettify
-            var html = ' ' + "\n" + '** TOP CURRENT **' + "\n";
+            var html = ' ' + "\n" + "** TOP (aujourd'hui) **" + "\n";
             _.forEach(data, function(user, idx) {
                 html += '[' + (idx+1) + '] ' + user.name + ' (' + user.pts + 'pts)' + "\n";
             });
             message.channel.send(html);
         });
-    } else if (message.content === '!ffbetoplast') {
+    } else if (message.content === '!ffbe top yesterday') {
         redis.get('top-last',function(err, data) {
             if (data) {
                 data = JSON.parse(data);
                 // prettify
-                var html = ' ' + "\n" + '** TOP LAST **' + "\n";
+                var html = ' ' + "\n" + '** TOP (hier) **' + "\n";
                 _.forEach(data, function(user, idx) {
                     html += '[' + (idx+1) + '](' + user.pos + ') ' + user.name + ' (' + user.pts + 'pts)' + "\n";
                 });
             } else {
-                var html = 'No Top Last found!';
+                var html = "Aucun classement disponible pour l'instant. Attendez minuit !";
             }
             message.channel.send(html);
         
         });
     } else if (message.content === '!ffbecurrentclear') {
         resetTopCurrent();
-        message.channel.send('Top Current Cleared!');
+        message.channel.send("TOP (aujourd'hui) effacé !");
     } else if (message.content === '!ffbelastclear') {
         resetTopLast();
-        message.channel.send('Top Last Cleared!');
+        message.channel.send('TOP (hier) effacé !');
     } else if (!message.author.bot) {
         // update top
         redis.get('top-current', function(err, data) {
