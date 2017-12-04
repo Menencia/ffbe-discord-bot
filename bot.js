3be9301b1caf9ea938dbbb86b20c2d72149cfaf3
Menencia <menencia@gmail.com>
2017-12-04 20:52:31 +0100

    improve welcome messages

--- bot.js
+++ bot.js
@@ -22,28 +22,28 @@ var ROLE_GUARDIANS = '379255305009102848';
 var lock = false;
 
 var messages = [
+    "On ne présente plus %user, 4* natif game-breaker. Faites de la place svp.",
     "Le grand %user vient d'arriver !",
     "Bienvenue, %user. On espère que tu as apporté de la pizza.",
     "C'est %user ! Loué soit le soleil ! o/",
-    "C'est un oiseau ! C'est un avion ! J'ai rien dit, c'est juste %user.",
-    "%user vient de rejoindre le serveur. Tout le monde, faites semblant d'être occupés !",
-    "%user vient de glisser dans le serveur.",
-    "%user a bondi dans le serveur. Kangourou !!",
+    "C'est un chocobo ! C'est un aéronef ! J'ai rien dit, c'est juste %user.",
+    "%user a traversé tout le continent de Lapis pour venir jusqu'ici !",
+    "%user, un habitant de Paladia, a rejoint notre serveur ! Soyez pas timides :)",
     "Bienvenue, %user. Nous vous attendions ( ͡° ͜ʖ ͡°)",
-    "Hé ! Écoutez ! %user nous a rejoint !",
-    "Oh mon dieu ! %user est là.",
+    "Hé ! Écoutez ! %user nous a rejoint ! Il apporte la chance avec lui. C'est le moment de puller !",
+    "Oh mon dieu ! %user est là. Tout le monde, faites semblant d'être occupés !",
     "%user est arrivé(e). La fête est terminée.",
     "Bienvenue %user. Laissez votre vie sociale près de la porte.",
-    "Un %user a spawn dans le serveur.",
+    "Un %user de rang supérieur a spawn dans le serveur.",
     "Tenez-vous bien. %user a rejoint le serveur.",
-    "%user est ici pour botter des fesses et mâcher du chewing-gum. Et %user est à court de chewing-gum.",
-    "%user a rejoint le serveur ! C’est super efficace !",
+    "%user est ici pour utiliser sa LB et tous nous sauver ! Hourra !",
+    "%user a rejoint le serveur ! Et il a plein de tickets 10+1 à distribuer !",
     "%user est ici, comme la prophétie l'avait annoncé.",
-    "Swoooosh. %user vient juste d’atterrir.",
-    "Les roses sont rouges, les violettes sont bleues, %user a rejoint ce lieu",
-    "%user vient d’arriver. Il est trop OP - il faut le nerf.",
-    "Bienvenue, %user. Reste un peu et écoute.",
-    "Un %user sauvage apparaît."
+    "Swoooosh. %user vient juste d’atterrir. Il fait 23° dehors et c'est plutôt ensoleillé.",
+    "Les roses sont rouges, les violettes sont bleues, %user a rejoint ce monde d'illusions.",
+    "%user vient d’arriver. Il est trop OP, il faut le nerf.",
+    "Un cristal arc-en-ciel descend ! OMG c'est %user !",
+    "Un %user sauvage apparaît. Sa TMR est excellente. Attrapez-le !"
 ];
 
 bot.on('ready', function () {
